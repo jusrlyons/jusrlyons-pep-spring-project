2d7c49ffe090e4b198b7ecc22e8ededce6c3df78
Jusdan Lyons <jusrlyons@comcast.net>
2025-04-08 15:02:05 +0000

    Submission

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -27,11 +27,11 @@ public class SocialMediaController {
     @PostMapping("/register")
     public ResponseEntity<Account> register(@RequestBody Account account) {
         Account registeredAccount = accountService.register(account.getUsername(), account.getPassword());
-        //register method in AccountService returns null if validation logic fails
+        
         if (registeredAccount == null && accountService.getAccountByUsername(account.getUsername()) != null) {
             return ResponseEntity.status(409).build();
         }
-        //returns differently if the cause of failure is the username being taken or anything else
+        
         else if (registeredAccount == null) {
             return ResponseEntity.status(400).build();
         }
@@ -41,7 +41,7 @@ public class SocialMediaController {
     @PostMapping("/login")
     public ResponseEntity<Account> login(@RequestBody Account account) {
         Account existingAccount = accountService.login(account.getUsername(), account.getPassword());
-        //The logic to check whether username and password match are in the AccountService login method
+        
         if (existingAccount != null) {
             return ResponseEntity.ok(existingAccount);
         }
@@ -53,7 +53,7 @@ public class SocialMediaController {
     @PostMapping("/messages")
     public ResponseEntity<Message> createMessage(@RequestBody Message message) {
         Message createdMessage = messageService.createMessage(message.getMessageText(), message.getPostedBy(), message.getTimePostedEpoch());
-        //createMessage method in MessageService will return null if validation logic fails
+        
         if (createdMessage == null) {
             return ResponseEntity.status(400).build();
         }
@@ -73,7 +73,7 @@ public class SocialMediaController {
     @DeleteMapping("/messages/{message_id}")
     public ResponseEntity<Integer> deleteMessageById(@PathVariable("message_id") int id) {
         boolean isDeleted = messageService.deleteMessageById(id);
-        //deleteMessageById method in MessageService checks if account with id does exist
+        
         if (isDeleted == true) {
             return ResponseEntity.ok(1);
         }
@@ -84,7 +84,7 @@ public class SocialMediaController {
 
     @PatchMapping("/messages/{message_id}")
     public ResponseEntity<Integer> updateMessageById(@PathVariable("message_id") int id, @RequestBody Message message) {
-        //updateMessageById method in MessageService contains logic to make sure update is valid
+        
         if (messageService.updateMessageById(id, message.getMessageText()) == false) {
             return ResponseEntity.status(400).build();
         }
